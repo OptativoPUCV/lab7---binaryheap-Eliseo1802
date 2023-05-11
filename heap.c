0e9f6a1c69b04d4ca47ae322a20f45c09b74042b
Replit user <>
2023-05-11 03:22:14 +0000

    new try

--- heap.c
+++ heap.c
@@ -59,8 +59,8 @@ void reorderHeapDown(Heap* pq, int index){
   left = (2*index)+1;
   right = (2*index)+2;
   
-  //if(left<pq->size && right<pq->size)
-    //return;
+  if(left<pq->size && right<pq->size)
+    return;
 
   if(pq->heapArray[left].priority>pq->heapArray[index].priority){
     swap(pq->heapArray, left, index);
@@ -75,8 +75,8 @@ void reorderHeapDown(Heap* pq, int index){
 
 void heap_pop(Heap* pq){
   swap(pq->heapArray,0,pq->size-1);
-  reorderHeapDown(pq,0);
   pq->size--;
+  reorderHeapDown(pq,0);
 }
 
 Heap* createHeap(){
