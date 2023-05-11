3c3dcecadf4dd78ec76e2ecb181206277f8ed45d
Replit user <>
2023-05-11 02:29:41 +0000

    new try

--- heap.c
+++ heap.c
@@ -36,7 +36,7 @@ Heap* createHeap(){
   Heap* new = (Heap*)malloc(sizeof(Heap));
   new->size = 0;
   new->capac = 3;
-  new->heapArray = malloc(sizeof(heapElem));
+  new->heapArray = (heapElem*)calloc(new->capac,sizeof(heapElem));
 
   return new;
 }
