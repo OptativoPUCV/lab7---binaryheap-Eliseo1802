5e46d0289101facc077adcdeeb4d85f3f43d795f
Replit user <>
2023-05-11 02:50:34 +0000

    new try

--- heap.c
+++ heap.c
@@ -16,19 +16,37 @@ typedef struct Heap{
   int capac;
 } Heap;
 
-
 void* heap_top(Heap* pq){
   void* pr = pq->heapArray[0].data;
   if(pq->size==0) return NULL;
   return pr;
 }
 
+void reorderHeap(Heap* pq, int index){
+  int parent = (index-1)/2;
+  int aux;
 
-
-void heap_push(Heap* pq, void* data, int priority){
-
+  if (pq->heapArray[parent].priority >= pq->heapArray[index].priority)
+    return;
+  else{
+    aux = pq->heapArray[parent].priority;
+    pq->heapArray[parent].priority = pq->heapArray[index].priority;
+    pq->heapArray[index].priority = aux;
+    reorderHeap(pq, parent);
+  }
 }
 
+void heap_push(Heap* pq, void* data, int priority){
+  if(pq->size==pq->capac){
+    pq->heapArray = realloc(pq->heapArray,sizeof(heapElem)*pq->capac*2);
+    pq->capac*=2;
+  }
+  pq->size++;
+  
+  pq->heapArray[pq->size-1].data = data;
+  pq->heapArray[pq->size-1].priority = priority;
+  reorderHeap(pq, pq->size-1);
+}
 
 void heap_pop(Heap* pq){
 
