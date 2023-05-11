260909569b97e8182cda174a1654ac6ef3123975
Replit user <>
2023-05-11 03:33:16 +0000

    new try

--- heap.c
+++ heap.c
@@ -59,9 +59,6 @@ void reorderHeapDown(Heap* pq, int index){
   left = (2*index)+1;
   right = (2*index)+2;
   
-  //if(left<pq->size && right<pq->size)
-    //return;
-
   if(pq->heapArray[left].priority>pq->heapArray[index].priority){
     swap(pq->heapArray, left, index);
     reorderHeapDown(pq,index);
